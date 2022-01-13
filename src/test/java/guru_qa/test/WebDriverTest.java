e6398b1012b2cc753a2f4958127906be985c04d8
evgeniashennikova <evgeniashenikova@gmail.com>
2022-01-14 02:40:15 +0300

    fix test

--- src/test/java/guru_qa/test/WebDriverTest.java
+++ src/test/java/guru_qa/test/WebDriverTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class WebDriverTest extends TestBase {
@@ -11,11 +12,19 @@ public class WebDriverTest extends TestBase {
     @Test
     void yaSearchTest() {
 
-        open("https://ya.ru/");
-        $("[name='text']").setValue("github").pressEnter();
-        $("#search-result").$(withText("Where the world builds software")).click();
-        switchTo().window(1);
-        assertThat(title()).isEqualTo("GitHub: Where the world builds software · GitHub");
+        step("Открыть сайт https://ya.ru/", () ->
+                open("https://ya.ru/"));
+
+        step("Ввести в строку поиска запрос 'github'", () ->
+                $("[name='text']").setValue("github").pressEnter());
+
+        step("Найти в результатах поиска сайт Github и открыть его", () -> {
+            $("#search-result").$(withText("Where the world builds software")).click();
+            switchTo().window(1);
+        });
+
+        step("Проверить текст заголовка страницы", () ->
+                assertThat(title()).isEqualTo("GitHub: Where the world builds software · GitHub"));
 
     }
 
