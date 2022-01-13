101445e56d46b16ffaed575d654e6af1e9c5d1c5
evgeniashennikova <evgeniashenikova@gmail.com>
2022-01-14 02:25:04 +0300

    fix

--- src/test/java/config/ApiConfig.java
+++ src/test/java/config/ApiConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "file:/temp/token.properties",
+        "file:/tmp/token.properties",
         "classpath:config/token.properties"
 })
 
